c17c5261d497f0965e813d944f31f17b1b13cb5b
AzaroTaro <amanhmedzahir@hotmail.com>
2020-05-15 11:36:14 +0530

    Cleanup and Lint
    We now seperate module (.py) files corresponding to the bot they interact with.

--- quotly.py
+++ quotly.py
@@ -1,3 +1,6 @@
+# Meme Bots Helper Extension for PaperplaneRemix.
+# Abstracted conversation with @QuotLyBot.
+
 import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -6,38 +9,42 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import client
 from userbot.utils.events import NewMessage
 
-plugin_category = "StickerizerMemes"
+plugin_category = "misc"
 
-@client.onMessage(command=("quotly", plugin_category),
+
+@client.onMessage(command=("quote", plugin_category),
                   outgoing=True,
-                  regex=r"(q|quo)tly(?: |$|\n)([\s\S]*)")
+                  regex=r"q(uote)?$")
 async def quotly(event: NewMessage.Event) -> None:
-    """Stickerize a message using Quotly"""
+    """Stickerize a message using @QuotLyBot"""
     await event.get_reply_message()
     if event.fwd_from:
-        return 
+        return
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
+        await event.edit("```Reply to any user message.```")
+        return
+    reply_message = await event.get_reply_message()
     if not reply_message.text:
-       await event.edit("```Reply to text message```")
-       return
+        await event.edit("```Reply to text message```")
+        return
     chat = "@QuotLyBot"
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
+        await event.edit("```Reply to actual users message.```")
+        return
     await event.edit("```Making a Quote```")
     async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please use /start on @QuotLyBot first```")
-              return
-          if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1031952739))
+            await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Please use /start on @QuotLyBot first```")
+            return
+        if response.text.startswith("Hi!"):
+            await event.edit(
+                "```Can you kindly disable your forward privacy settings for good?```"
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
